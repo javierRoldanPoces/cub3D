b623c8d3e72b13ff0c2d22c955e288d9d9a0844b
Ismael <imoro-sa@student.42malaga.com>
2023-11-01 18:25:57 +0100

    parsing seems ok

--- src/parse/check_map.c
+++ src/parse/check_map.c
@@ -23,14 +23,14 @@ void	check_map(t_map *map)
 		n++;
 	}
 	if (player == 0 || wall == 0 || map->heigth < 3 || map->width < 3)
-		ft_error("invalid map design0", NULL);
+		ft_error("invalid map design", NULL);
 }
 
 void	check_valid_char(char c, int *wall)
 {
 	if (c != ' ' && c != '1' && c != '0'
 		&& c != 'N' && c != 'S' && c != 'W' && c != 'E')
-		ft_error("invalid map design1", NULL);
+		ft_error("invalid map design", NULL);
 	if (c == '1')
 		*wall = 1;
 }
@@ -41,7 +41,7 @@ void	check_player_position(t_map *map, int n, int i, int *player)
 		|| map->matrix[n][i] == 'W' || map->matrix[n][i] == 'E')
 	{
 		if (*player != 0)
-			ft_error("invalid map design2", NULL);
+			ft_error("invalid map design", NULL);
 		else
 		{
 			*player = 1;
@@ -56,7 +56,7 @@ void	check_walls(t_map *map, int n, int i)
 	{
 		if (n == 0 || n == map->heigth - 1
 			|| i == 0 || i == map->width - 1)
-			ft_error("invalid map design7", NULL);
+			ft_error("invalid map design", NULL);
 		check_sides(map, n, i);
 	}
 }
@@ -65,14 +65,14 @@ void	check_sides(t_map *map, int n, int i)
 {
 	if (n > 0)
 		if (map->matrix[n - 1][i] == ' ')
-			ft_error("invalid map design3", NULL);
+			ft_error("invalid map design", NULL);
 	if (n < map->heigth)
 		if (map->matrix[n + 1][i] == ' ')
-			ft_error("invalid map design4", NULL);
+			ft_error("invalid map design", NULL);
 	if (i > 0)
 		if (map->matrix[n][i - 1] == ' ')
-			ft_error("invalid map design5", NULL);
+			ft_error("invalid map design", NULL);
 	if (i < map->width)
 		if (map->matrix[n][i + 1] == ' ')
-			ft_error("invalid map design6", NULL);
+			ft_error("invalid map design", NULL);
 }
